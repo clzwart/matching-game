a34891c8fc969af04bc6405a532d71db1e89e9c0
unknown <131737935+clzwart@users.noreply.github.com>
2025-07-28 17:58:53 -0700

    if two selected cards don't match they return to face down

--- script.js
+++ script.js
@@ -5,18 +5,24 @@ document.querySelectorAll(".card").forEach((element) => {
 function flipCard(event) {
   event.target.classList.remove("face-down");
   event.target.classList.add("selected");
+  const cards = document.querySelectorAll(".card");
   const cardType = event.target.dataset.cardType;
+  const selectedCards = document.querySelectorAll(".selected");
   const matchingSelectedCards = document.querySelectorAll(
     `.selected[data-card-type="${cardType}"]`
   );
 
+  if (selectedCards.length === 2 && matchingSelectedCards.length < 2) {
+    cards.forEach((element) => {
+      element.classList.add("face-down");
+      element.classList.remove("selected");
+    });
+  }
+
   if (matchingSelectedCards.length % 2 === 0) {
     matchingSelectedCards.forEach((element) => {
       element.classList.add("hidden");
-    });
-  } else {
-    matchingSelectedCards.forEach((element) => {
-      element.classList.add("face-down");
+      element.classList.remove("selected");
     });
   }
 }
